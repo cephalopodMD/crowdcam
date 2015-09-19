6a36ce2fcc21139c93065c99a252e07d95b297ea
cephalopodMD <mistahowe@gmail.com>
2015-09-19 15:09:30 -0400

    index page

--- crowdcamapp/__init__.py
+++ crowdcamapp/__init__.py
@@ -1,3 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return '''
+    <!doctype html>
+    <title>CrowdCam</title>
+    <h1>Document the world together</h1>
+    '''
